e0d9c770c5c02a398157df9a52342d690d560067
Abdourahamane Fall <2944063+sobelfallcayor@users.noreply.github.com>
2026-04-10 14:04:48 -0400

    Create users.fixture.ts

---
+++ users.fixture.ts
@@ -0,0 +1,5 @@
+// src/testing/fixtures/users.fixture.ts
+export const USERS_FIXTURE = [
+  { id: 1, name: 'Alice', email: 'alice@test.com' },
+  { id: 2, name: 'Bob',   email: 'bob@test.com'   },
+];
